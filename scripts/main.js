b37b92324c9e052440d389b53ccd1d5b40a8405e
NovaDioChan <84956725+NovaDioChan@users.noreply.github.com>
2023-01-27 13:53:10 +0500

    Add new js file
    And it's don't work ^-^

---
+++ scripts/main.js
@@ -0,0 +1,7 @@
+function hey(){
+    let mainS = document.getElementById(main);
+    let startS=document.getElementById(start);
+    mainS.style.opacity =100;
+    startS.style.opacity =0;
+    alert('Hello world');
+}
